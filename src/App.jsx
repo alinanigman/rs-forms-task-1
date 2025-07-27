cf07635caaa0ff59fddc9ab5751056e71593a066
alinakassym <alinakassym@gmail.com>
2025-07-27 20:21:37 +0500

    feat: add email validation

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,32 @@ import { useState } from "react";
 import styles from "./App.module.css";
 
 function App() {
-  const [email] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(null);
+
+  const onEmailChange = ({ target }) => {
+    setEmail(target.value);
+    let error = null;
+    const emailRegex = /^[\w_-]+@[\w_-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(target.value)) {
+      error = "Email не корректен";
+    }
+    setError(error);
+  };
 
   return (
     <div className={styles.App}>
       <form className={styles.form}>
-        <input type="text" name="email" value={email} placeholder="Логин" />
+        <div className={styles.error}>
+          {!!error && <p className={styles.errorText}>{error}</p>}
+        </div>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          placeholder="Логин"
+          onChange={onEmailChange}
+        />
         <button>Зарегистрироваться</button>
       </form>
     </div>
